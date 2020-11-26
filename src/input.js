3712cb4392703ee88bf65435ddc7d6d04e5c201a
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-11-26 20:54:37 -0300

    add transition to label

--- src/input.js
+++ src/input.js
@@ -4,7 +4,7 @@ const template = () => {
   var template = document.createElement("template")
   let node = `
     <div class="container">
-      <label for="custom-input" class="container__label">custom input</label>
+      <label for="custom-input" class="container__label" id="label">custom input</label>
       <input type="text" name="custom-input" id="custom-input" class="container__input" />
     </div>
   `
@@ -12,9 +12,25 @@ const template = () => {
   template.innerHTML = node
   return template.content.firstChild
 }
+const inputBehavior = () => {
+  
+  let inputEl = document.getElementById("custom-input")
+  inputEl.addEventListener("focus", (e) => {
+    let labelEl = document.getElementById("label")
+    labelEl.style.top = "-8px"
+  })
+  inputEl.addEventListener("blur", (e) => {
+    let labelEl = document.getElementById("label")
+    if (!e.target.value) {
+      labelEl.style.top = "10px"
+    }
+   
+  })
+}
 const input = () => {
   const inputWrapper = document.getElementById("input")
   inputWrapper.appendChild(template())
+  inputBehavior()
 }
 
 input()
