2d976e38377e6a6d39b826413d2e29806ad78583
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-11-27 15:35:00 -0300

    implement to way databinder

--- src/input.js
+++ src/input.js
@@ -1,36 +1,54 @@
 import "./__input.scss" 
-
+var obj = {}
+obj.message = ''
 const template = () => {
   var template = document.createElement("template")
   let node = `
     <div class="container">
       <label for="custom-input" class="container__label" id="label">custom input</label>
-      <input type="text" name="custom-input" id="custom-input" class="container__input" />
+      <input value="${obj.message}" type="text" name="custom-input" id="custom-input" class="container__input" />
+      <p id="message"></p>  
     </div>
   `
   node = node.trim()
   template.innerHTML = node
   return template.content.firstChild
 }
-const inputBehavior = () => {
+const towWayDataBind = (inputEl) => {
+  inputEl.addEventListener("input", (e) => {
+    obj.message = e.target.value
+    let pEl = document.getElementById("message")
+    pEl.innerHTML = obj.message
+  })
+}
+const inputUnfocus = (inputEl) => {
   
-  let inputEl = document.getElementById("custom-input")
   inputEl.addEventListener("focus", (e) => {
     let labelEl = document.getElementById("label")
-    labelEl.style.top = "-8px"
+    labelEl.classList.remove('container__label--down')
+    labelEl.classList.add('container__label--up')
   })
+}
+const inputFocus = (inputEl) => {
   inputEl.addEventListener("blur", (e) => {
     let labelEl = document.getElementById("label")
     if (!e.target.value) {
-      labelEl.style.top = "10px"
+      labelEl.classList.remove('container__label--up')
+      labelEl.classList.add('container__labe--down')
     }
    
   })
 }
+const inputActions = () => {
+  let inputEl = document.getElementById("custom-input")
+  inputFocus(inputEl)
+  inputUnfocus(inputEl)
+  towWayDataBind(inputEl)
+}
 const input = () => {
   const inputWrapper = document.getElementById("input")
   inputWrapper.appendChild(template())
-  inputBehavior()
+  inputActions()
 }
 
 input()
